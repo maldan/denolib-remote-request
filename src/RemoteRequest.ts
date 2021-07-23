0caecc04d08e6d6964998dc2b9e77229db32eecd
Roman <blackwanted@yandex.ru>
2021-07-23 11:40:37 +0300

    Update

--- src/RemoteRequest.ts
+++ src/RemoteRequest.ts
@@ -1,20 +1,22 @@
 export class RemoteRequest {
-  /**
+    /**
      * Get text from URL
-     * @param path 
-     * @param args 
+     * @param path
+     * @param args
      */
-  static async get<T>(
-    path: string,
-    args: { [x: string]: unknown } = {},
-  ): Promise<T> {
-    const request = await fetch(path);
-    if (
-      request.headers.get("content-type")?.toString().match("application/json")
-    ) {
-      return await request.json() as unknown as T;
-    } else {
-      return await request.text() as unknown as T;
+    static async get<T>(path: string, args: { [x: string]: unknown } = {}): Promise<T> {
+        const request = await fetch(path);
+        const contentType = request.headers.get("content-type")?.toString() ?? "";
+
+        if (contentType.match("application/json")) {
+            return ((await request.json()) as unknown) as T;
+        } else {
+            return ((await request.text()) as unknown) as T;
+        }
     }
-  }
+
+    static async getBinary(path: string, args: { [x: string]: unknown } = {}): Promise<Uint8Array> {
+        const request = await fetch(path);
+        return new Uint8Array(await (await request.blob()).arrayBuffer());
+    }
 }
--- test/base.test.ts
+++ test/base.test.ts
@@ -4,10 +4,8 @@ import { RemoteRequest } from "./../mod.ts";
 
 // Simple name and function, compact form, but not configurable
 Deno.test("hello world #1", async () => {
-  await RemoteRequest.get(
-    "https://developer.mozilla.org/en-US/docs/Web/API/Body/text",
-  );
+    await RemoteRequest.get("https://developer.mozilla.org/en-US/docs/Web/API/Body/text");
 
-  const x = 1 + 2;
-  assertEquals(x, 3);
+    const x = 1 + 2;
+    assertEquals(x, 3);
 });
